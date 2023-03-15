929530a3758964332270be8274137c0ba09fb3ab
Max <maxime.valle@cegetel.net>
2023-03-15 16:05:11 +0100

    OK

--- GenerateData.py
+++ GenerateData.py
@@ -4,24 +4,24 @@ import matplotlib.pyplot as plt
 import torch
 from torch.utils.data import TensorDataset
 
-def GenerateData():
+def GenerateData(nb_points):
 
     ### statistical repartition of groups points
     mu_A = np.array([[3],[3]])
-    sigma_A = 0.5
+    sigma_A = np.array([[0.5],[0.5]])
+    print(type(sigma_A))
 
     mu_B = np.array([[0],[2]])
-    sigma_B = 0.3
+    sigma_B = np.array([[0.9],[0.3]])
 
     mu_C = np.array([ [2] ,[0] ])
-    sigma_C = 0.7
-
+    sigma_C = np.array([[0.7],[0.7]])
 
     # La convention Pytorch exige qu'on ait un élément par ligne => pour dire qu'on ne doit pas structurer les état d'un élément par colonne, mais je ne le fais pas, j'inverserai plus tard
     ### generation of 50 points of each of the 3 groups
-    X_A = sigma_A*np.random.randn(2,50) + mu_A
-    X_B = sigma_B*np.random.randn(2,50) + mu_B
-    X_C = sigma_C*np.random.randn(2,50) + mu_C
+    X_A = sigma_A*np.random.randn(2,nb_points) + mu_A
+    X_B = sigma_B*np.random.randn(2,nb_points) + mu_B
+    X_C = sigma_C*np.random.randn(2,nb_points) + mu_C
 
     Y_A = 0 * np.ones( (X_A.shape[1],1) ).squeeze()
     Y_B = 1 * np.ones( (X_B.shape[1],1) ).squeeze()
@@ -31,9 +31,9 @@ def GenerateData():
     
     ### visualisating the data 
     fig, ax = plt.subplots()
-    plt.scatter(X_A[0,:],X_A[1,:], label = "group A")
-    plt.scatter(X_B[0,:],X_B[1,:], label = "group B")
-    plt.scatter(X_C[0,:],X_C[1,:], label = "group C")
+    plt.scatter(X_A[0,:],X_A[1,:], label = "group A", color = "blue")
+    plt.scatter(X_B[0,:],X_B[1,:], label = "group B", color = "orange")
+    plt.scatter(X_C[0,:],X_C[1,:], label = "group C", color = "green")
     plt.legend()
     ax.set_xlabel('x_1'), ax.set_ylabel('x_2')
     #ax.set_xlim((-5,10)), ax.set_ylim((0,10))
@@ -55,14 +55,8 @@ def GenerateData():
     X_train = X_train.T
     Y_train = Y_train.T
     train_ds = TensorDataset(X_train, Y_train)          # the TensorDatset structure allows to associate a point with its corresponding output 
-   
-    ### test with the TensorDatset structure
-    # bs = 3
-    # xb,yb = train_ds[0:bs]
-    # print(f'\n the first 3 points of the dataset : \n {xb}')
-    # print(f'\n the associated outputs of those points 3 : \n {yb}')
 
     return train_ds
 
 if __name__ == "__main__":
-    GenerateData()
+    GenerateData(500)
---
+++ README.md
@@ -0,0 +1,32 @@
+### Classification of 3 groups of points using a Pytorch neural network
+
+The objective was to separate the 3 groups of points displayed below (group A, B, C) using a neural network.
+
+![dataset](./figures/dataset.png "dataset : 3 groups of points to classify")
+
+The `GenerateData()` function is used to randomly create the datset. This function returns a TensorDataset.
+Because of the way the `TensorDataset()` constructor is made, each data of the sample has been positionned in row. One row per data, and one column per dimension.
+So in the TensorDataset returned object, the data are organised as follow : 
+
+$$X_{train} = 
+\left(\begin{array}{cc} 
+x_{1,1} & ... & x_{1,m} \\
+x_{2,1} & ... & x_{2,m}
+\end{array}\right)^T
+= 
+\left(\begin{array}{cc} 
+x_{1,1} & x_{2,1} \\
+... & ... \\
+x_{1,m} & x_{2,m}
+\end{array}\right)
+$$ 
+
+And ${Y_{train}}$ must be 1D.
+
+The neural network is optimised so that the cost function decrease.
+
+![cost function over iterations](./figures/cost_fun.png "cost function over iterations")
+
+Once the network is trained, it's capable of classify the points regarding the patterns described in the following figure : 
+
+![neural network contours](./figures/points_classification.png "neural network contours")
